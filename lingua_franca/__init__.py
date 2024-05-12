9288f078498fb5badf25dcc8b2027c6836d908cf
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-05-12 21:38:23 +0100

    feat/ovos_defaults (#68)

--- lingua_franca/__init__.py
+++ lingua_franca/__init__.py
@@ -4,3 +4,17 @@ from .internal import get_default_lang, set_default_lang, get_default_loc, \
     load_languages, unload_language, unload_languages, get_supported_langs
 
 from lingua_franca import config
+
+# honor the global OVOS language preferences unless configured to do otherwise
+if config.ovos_defaults:
+    try:
+        from ovos_config.locale import setup_locale
+        from ovos_config.config import Configuration
+        # if running in a OVOS system, set default lang/timezone from config file
+        setup_locale()
+        # pre load all secondary_langs
+        secondary_langs = Configuration().get("secondary_langs", [])
+        if secondary_langs:
+            load_languages(secondary_langs)
+    except ImportError:
+        pass
--- lingua_franca/config.py
+++ lingua_franca/config.py
@@ -1,2 +1,3 @@
 load_langs_on_demand = False
 inject_timezones = True
+ovos_defaults = True  # use mycroft.conf for default values
